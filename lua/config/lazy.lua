0ffc9266bcb999fa70169bc5083e256005cfea72
naresh-khatri <naresh.khatri2345@gmail.com>
2024-10-04 12:13:21 +0530

    feat: disable toggleterm

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,7 +21,7 @@ require("lazy").setup({
     -- import any extra modules here
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "akinsho/toggleterm.nvim" },
+    -- { import = "akinsho/toggleterm.nvim" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
